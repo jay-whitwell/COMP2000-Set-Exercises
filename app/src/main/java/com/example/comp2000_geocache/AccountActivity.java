0c88e743bba769bbe19e8ee7fefd8d3e2598f888
Jacob Whitwell <jacob.whitwell@students.plymouth.ac.uk>
2021-11-03 16:01:44 +0000

    Added many new maps and buttons.

--- app/src/main/java/com/example/comp2000_geocache/AccountActivity.java
+++ app/src/main/java/com/example/comp2000_geocache/AccountActivity.java
@@ -2,7 +2,10 @@ package com.example.comp2000_geocache;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -10,5 +13,51 @@ public class AccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+        Button accountMapButton = findViewById(R.id.accountMapButton);
+        Button accountStatisticsButton = findViewById(R.id.accountStatisticsButton);
+        Button accountManageCachesButton = findViewById(R.id.accountManageCaches);
+
+        accountMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                map();
+            }
+        });
+
+        accountStatisticsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                statistics();
+            }
+        });
+
+        accountManageCachesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manageCache();
+            }
+        });
+    }
+
+    public void map()
+    {
+        Intent intent = new Intent(this, MapActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void statistics()
+    {
+        Intent intent = new Intent(this, StatisticsActivity.class);
+
+        startActivity(intent);
+    }
+
+    public void manageCache()
+    {
+        Intent intent = new Intent(this, ManageCacheActivity.class);
+
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/comp2000_geocache/ManageCacheActivity.java
@@ -0,0 +1,14 @@
+package com.example.comp2000_geocache;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageCacheActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_cache);
+    }
+}
